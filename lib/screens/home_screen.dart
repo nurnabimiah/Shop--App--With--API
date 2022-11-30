b12065e59a4c83bc732d52b1de309e88e7c004e2
nurnabimiah <diunayonahmed@gmail.com>
2022-11-30 23:03:39 +0600

    Search bar design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:store_api_flutter_course/consts/global_colors.dart';
 import 'package:store_api_flutter_course/screens/user_screen.dart';
 
 
@@ -14,6 +15,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _textEditingController;
+
+  @override
+  void initState() {
+    _textEditingController = TextEditingController();
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -40,7 +55,38 @@ class _HomeScreenState extends State<HomeScreen> {
                 icon: IconlyBold.user3)
           ],
         ),
-        body: Center(child: Text("Welcome to this course")),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              SizedBox(height: 15,),
+              Padding(
+                padding: const EdgeInsets.only(left: 8,right: 8),
+                child: TextField(
+                  controller: _textEditingController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    filled: true,
+                    fillColor: Theme.of(context).cardColor,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                      borderSide: BorderSide(color: Theme.of(context).cardColor)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide(
+                        width: 1,
+                        color: Theme.of(context).colorScheme.secondary
+                      )
+                    ),
+                    suffixIcon: Icon(IconlyLight.search,color: lightIconsColor,)
+                  ) ,
+                ),
+              )
+            ],
+          ),
+        )
       ),
     );
   }
